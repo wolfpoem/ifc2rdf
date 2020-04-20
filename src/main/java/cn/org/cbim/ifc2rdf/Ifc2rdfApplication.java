9fa11fa63e91007b5cda5450a80631320c645c76
ninggx <ninggx@cadg.cn>
2020-04-20 14:35:09 +0800

    Initial commit

---
+++ src/main/java/cn/org/cbim/ifc2rdf/Ifc2rdfApplication.java
@@ -0,0 +1,13 @@
+package cn.org.cbim.ifc2rdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ifc2rdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ifc2rdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/org/cbim/ifc2rdf/Ifc2rdfApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.org.cbim.ifc2rdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ifc2rdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
